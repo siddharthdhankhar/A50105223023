e518f17ad835c519c63d12634839082ab3e69907
siddharthdhankhar <dhankharsiddharth3@gmail.com>
2026-05-29 13:13:04 +0530

    refreshing the access token

--- logging_middleware/index.js
+++ logging_middleware/index.js
@@ -1,5 +1,5 @@
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzaWRkaGFydGg4QHMuYW1pdHkuZWR1IiwiZXhwIjoxNzgwMDM4MjMwLCJpYXQiOjE3ODAwMzczMzAsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiIyZmVmYWEyZS1iMDNhLTRiNDUtYjQyOC0zZWQ0YzQyMTNkYWEiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJzaWRkaGFydGgiLCJzdWIiOiJiZjczYTNhNC0zZWM1LTRjOGQtODIwZS0xMDM0YmNiYmE3MGUifSwiZW1haWwiOiJzaWRkaGFydGg4QHMuYW1pdHkuZWR1IiwibmFtZSI6InNpZGRoYXJ0aCIsInJvbGxObyI6ImE1MDEwNTIyMzAyMyIsImFjY2Vzc0NvZGUiOiJKR0pzVVQiLCJjbGllbnRJRCI6ImJmNzNhM2E0LTNlYzUtNGM4ZC04MjBlLTEwMzRiY2JiYTcwZSIsImNsaWVudFNlY3JldCI6ImdzVXVlUFZ0V2h1Zlp3UnEifQ.cFipoS4N28REtNczZrkL_l68FCKrVlDr9Soksl7jWxM";
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzaWRkaGFydGg4QHMuYW1pdHkuZWR1IiwiZXhwIjoxNzgwMDQwMjA4LCJpYXQiOjE3ODAwMzkzMDgsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJiMWVkN2MwOS0yZGUzLTQ3YTYtOGVjZS00ZTMwY2U0MTZkOWUiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJzaWRkaGFydGgiLCJzdWIiOiJiZjczYTNhNC0zZWM1LTRjOGQtODIwZS0xMDM0YmNiYmE3MGUifSwiZW1haWwiOiJzaWRkaGFydGg4QHMuYW1pdHkuZWR1IiwibmFtZSI6InNpZGRoYXJ0aCIsInJvbGxObyI6ImE1MDEwNTIyMzAyMyIsImFjY2Vzc0NvZGUiOiJKR0pzVVQiLCJjbGllbnRJRCI6ImJmNzNhM2E0LTNlYzUtNGM4ZC04MjBlLTEwMzRiY2JiYTcwZSIsImNsaWVudFNlY3JldCI6ImdzVXVlUFZ0V2h1Zlp3UnEifQ.LY0QIJEU1NrAyM3ZsIeexkeyeanFRHMTOVbC0N5BpY0";
 const API_URL = "http://4.224.186.213/evaluation-service/notifications";
 
 async function Log(stack, level, pkgName, message) {
@@ -8,7 +8,7 @@ async function Log(stack, level, pkgName, message) {
         return; 
     }
 
-    const url = "http://4.224.186.213/evaluation-service/logs";
+    const url = typeof window !== 'undefined' ? '/evaluation-service/logs' : 'http://4.224.186.213/evaluation-service/logs';
 
     const payload = {
         stack: stack.toLowerCase(),
@@ -36,4 +36,10 @@ async function Log(stack, level, pkgName, message) {
     }
 }
 
-module.exports = { Log };
+// Universal Export for both Node.js and React
+if (typeof module !== 'undefined') {
+    module.exports = { Log };
+}
+if (typeof window !== 'undefined') {
+    window.Log = Log;
+}
